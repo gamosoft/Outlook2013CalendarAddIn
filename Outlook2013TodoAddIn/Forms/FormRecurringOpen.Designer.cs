933271246c8596326b9ec7a46291f0833d0a2e03
gamosoft_cp <SND\gamosoft_cp>
2014-10-31 15:37:26 +0000

    Added context menu option to delete appointments

--- Outlook2013TodoAddIn/Forms/FormRecurringOpen.Designer.cs
+++ Outlook2013TodoAddIn/Forms/FormRecurringOpen.Designer.cs
@@ -106,6 +106,7 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Open Recurring Item";
+            this.Load += new System.EventHandler(this.FormRecurringOpen_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
